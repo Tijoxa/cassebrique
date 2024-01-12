352b8473eac16ef379769f3b6a85a0cde545998c
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2024-01-12 16:48:32 +0100

    Update NUMBER_BRICKS constant and adjust brick positions

--- src/brick.rs
+++ src/brick.rs
@@ -9,7 +9,7 @@ pub struct Brick {
     pub hp: i32,
 }
 pub const BRICK_DIMENSION: Vec2 = Vec2 { x: 60., y: 20. };
-pub const NUMBER_BRICKS: usize = 1;
+pub const NUMBER_BRICKS: usize = 20;
 
 pub fn get_brick_radius() -> f32 {
     BRICK_DIMENSION.x.min(BRICK_DIMENSION.y) / 3.
@@ -27,7 +27,18 @@ pub fn setup_brick(mut commands: Commands) {
         ..shapes::RoundedPolygon::default()
     };
 
-    for _i in 0..NUMBER_BRICKS {
+    let base_x = (-GAME_DIMENSION.x + THICKNESS + BRICK_DIMENSION.x) / 2. + 10.;
+    let base_y = (GAME_DIMENSION.y - THICKNESS - BRICK_DIMENSION.y) / 2. - 20.;
+
+    for i in 0..NUMBER_BRICKS {
+        let mut x_buffer = base_x + (i as f32) * (BRICK_DIMENSION.x + 15.);
+        let mut y_buffer = base_y;
+
+        while x_buffer > -base_x {
+            x_buffer += base_x;
+            y_buffer -= BRICK_DIMENSION.y + 25.;
+        }
+
         commands
             .spawn((ShapeBundle {
                 path: GeometryBuilder::build_as(&shape),
@@ -35,7 +46,7 @@ pub fn setup_brick(mut commands: Commands) {
             },))
             .insert(Fill::color(Color::CYAN))
             .insert(Brick { hp: 2 })
-            .insert(Transform::from_translation(Vec3::new(0., 0., 0.)));
+            .insert(Transform::from_translation(Vec3::new(x_buffer, y_buffer, 0.)));
     }
 }
 
